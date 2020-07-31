0ad357c709fde931db960f4136a41b0d4f1fcfba
zhangyirong123 <525386154@qq.com>
2020-07-31 13:07:39 +0800

    完成盖板测试

---
+++ src/main/java/com/MarketScores/Goods.java
@@ -0,0 +1,4 @@
+package com.MarketScores;
+
+public class Goods {
+}
--- src/test/java/com/MarketScores/GoodsTest.java
+++ src/test/java/com/MarketScores/GoodsTest.java
@@ -1,5 +1,21 @@
 package com.MarketScores;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class GoodsTest {
+    @Test
+    public void should_return_100_when_cosume_given_apple_100() {
+
+        //when
+        //given
+        Cosnumer consumer = new Consumer();
+        Goods apple = new Goods("apple");
+        consumer.consume(apple, 100);
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(Consumer.getConsumeRecords());
+        //then
+        Assert.assertEquals(100, result);
+    }
 
 }
