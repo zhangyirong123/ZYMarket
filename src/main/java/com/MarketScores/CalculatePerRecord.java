4580b4bdae08a2abbfafedc66dd04861b8344ee0
zhangyirong123 <525386154@qq.com>
2020-07-31 14:11:21 +0800

    重构单个物品积分实现

--- src/main/java/com/MarketScores/CalculatePerRecord.java
+++ src/main/java/com/MarketScores/CalculatePerRecord.java
@@ -1,9 +1,20 @@
 package com.MarketScores;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CalculatePerRecord {
     public int calculatePerRecord(ConsumeRecord consumeRecord) {
-        BasicScoresCalculator basicScoresCalculator = new BasicScoresCalculator();
-        ExtraScoresCalculator extraScoresCalculator = new ExtraScoresCalculator();
-        return basicScoresCalculator.ScoresCalculator(consumeRecord) + extraScoresCalculator.ScoresCalculator(consumeRecord);
+//        BasicScoresCalculator basicScoresCalculator = new BasicScoresCalculator();
+//        ExtraScoresCalculator extraScoresCalculator = new ExtraScoresCalculator();
+//        return basicScoresCalculator.ScoresCalculator(consumeRecord) + extraScoresCalculator.ScoresCalculator(consumeRecord);
+        int result = 0;
+        List<ScoresCalculator> scoresCalculators = new ArrayList<>(Arrays.asList(new BasicScoresCalculator(), new ExtraScoresCalculator()));
+        for (ScoresCalculator scoresCalculator : scoresCalculators
+        ) {
+            result += scoresCalculator.ScoresCalculator(consumeRecord);
+        }
+        return result;
     }
 }
